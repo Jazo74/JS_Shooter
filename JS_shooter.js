9703ee19aabafeeb04ad6cb8bd8f956c421e717c
Jazo74 <55430157+Jazo74@users.noreply.github.com>
2020-04-27 20:04:32 +0200

    x
    x

--- JS_shooter.js
+++ JS_shooter.js
@@ -3,8 +3,7 @@ let num = 1;
 let score = 0;
 let swoosh = document.getElementById('swoosh');
 
-
-
+// Main program
 function main() {
   //"global"
     const headerDiv = document.querySelector('.header')
@@ -12,22 +11,19 @@ function main() {
     const containerDiv = document.querySelector('.playground');
     const audio = new Audio('Audio/SuTurno.mp3')
     addAudioButtons(footerDiv, audio)
-      
-
-
     setInterval(() => {
     createATarget(containerDiv);
   }, 1000);
-
 }
 
+// Moving the targets
 function makeOneMove(num) {
   const target = document.getElementById(`target_${num}`);
   target.classList.remove('hide');
   target.style['margin-left'] = '1800px';
 }
 
-
+// This happens when a hit landed on a target
 function onHit(event) {
   const hitTarget = event.target
   swoosh.play();
@@ -37,43 +33,7 @@ function onHit(event) {
   document.getElementById("score").innerHTML = "Score: "  + score;
 }
 
-function createTarget(amount, containerDiv) {
-  var num = 1;
-
-  for (let i = 0; i < amount; i++) {
-    let rndVertical = Math.floor(Math.random() * 500);
-    const img = document.createElement('img')
-    img.setAttribute('class', 'target')
-    img.setAttribute('src', 'https://pngimg.com/uploads/coronavirus/coronavirus_PNG7.png')
-    img.setAttribute('id', `target_${num}`)
-    img.setAttribute('width', '300')
-    img.setAttribute('height', 'auto')
-    img.addEventListener('click', onHit, { once: true })
-    containerDiv.appendChild(img)
-    img.style['margin-top'] = rndVertical;
-    setTimeout(makeOneMove.bind(null, num), 1000);
-    num++
-  }
-
-}
-
-function createATarget(containerDiv) {
-  let rndVertical = Math.floor(Math.random() * 700);
-  const img = document.createElement('img')
-  img.setAttribute('class', 'target')
-  img.setAttribute('src', './covid.png')
-  img.setAttribute('id', `target_${num}`)
-  img.setAttribute('width', '230')
-  img.setAttribute('height', '150')
-  img.addEventListener('click', onHit, { once: true })
-  containerDiv.appendChild(img)
-  img.style['margin-top'] = rndVertical;
-  setTimeout(makeOneMove.bind(null, num), 50);
-  num++
-}
-
-
-
+// Creating audio buttons
 function addAudioButtons(footerDiv, audio) {
 
   const audioButton = document.createElement('button')
@@ -91,17 +51,17 @@ function addAudioButtons(footerDiv, audio) {
   const unMuteButton = document.createElement('button')
   unMuteButton.addEventListener('click', function() {
     video.muted = false;
-});
+  });
   unMuteButton.setAttribute('class', 'audioButton')
   unMuteButton.setAttribute('alt', 'click to stop sound effect')
-  unMuteButton.innerText = 'Mute sound'
+  unMuteButton.innerText = 'Mute sound  '
 
   footerDiv.appendChild(audioButton)
   footerDiv.appendChild(muteButton)
   footerDiv.appendChild(unMuteButton)
-
 }
 
+// Creating targets
 function createATarget(containerDiv) {
     let rndVertical = Math.floor(Math.random() * 650);
     let rndSpeed = 0;
@@ -115,22 +75,21 @@ function createATarget(containerDiv) {
     img.setAttribute('class', 'target')
     img.setAttribute('src', './covid.png')
     img.setAttribute('id', `target_${num}`)
-    img.setAttribute('width', '230')
-    img.setAttribute('height', '150')
-    img.setAttribute('height', '150')
+    img.setAttribute('width', '280')
+    img.setAttribute('height', 'auto')
     img.addEventListener('mouseover', onHit, { once: true })
     containerDiv.appendChild(img)
     img.style['margin-top'] = rndVertical;
     img.style.transitionDuration = `${rndSpeed}s`;
     setTimeout(makeOneMove.bind(null,num),50);
-    setTimeout(removeATarget.bind(null, `target_${num}`),5000);
+    setTimeout(removeATarget.bind(null, `target_${num}`),6000);
     num++
 }
 
+//Removing old targets
 function removeATarget(id){
-document.getElementById(id).remove();
+    document.getElementById(id).remove();
 }
 
-
-
+// Starting program
 main()
