f3e6f7ef9d2e2ac23e102519ef8121bbc57f26db
Erik-Aron-Szabo <erik.aron.szabo@gmail.com>
2020-04-27 14:05:47 +0200

    proba 1 make it move
    Co-authored-by: mezeigergely <mezei.gergely89@gmail.com>

--- JS_shooter.js
+++ JS_shooter.js
@@ -1,5 +1,3 @@
-const target = document.getElementsByClassName("target");
-target[0].style['margin-left'] = '1000px';
 
 
 
@@ -9,13 +7,22 @@ target[0].style['margin-left'] = '1000px';
 
 function main() {
   //"global"
-  const containerDiv = document.querySelector('.playground')
-
-
-
+  const containerDiv =document.querySelector('.playground')
 
 
   createTarget(5, containerDiv)
+  setTimeout(makeThemMove, 1000)
+
+
+}
+
+function makeThemMove() {
+  const targets = document.querySelectorAll(".target");
+  for (let i = 0; i < targets.length; i++) {
+    const element = targets[i];
+    element.classList.remove('hide')
+    element.style['margin-left'] = '1000px';
+  }
 }
 
 
@@ -28,7 +35,7 @@ function createTarget(amount, containerDiv) {
   var num = 1;
 
   for (let i = 0; i < amount; i++) {
-    
+
     const img = document.createElement('img')
     img.setAttribute('class', 'target')
     img.setAttribute('src', 'https://pngimg.com/uploads/coronavirus/coronavirus_PNG7.png')
@@ -36,6 +43,7 @@ function createTarget(amount, containerDiv) {
     img.setAttribute('width', '230')
     img.setAttribute('height', '150')
     img.addEventListener('click', onHit, { once: true })
+    img.classList.add('hide')
 
     containerDiv.appendChild(img)
     num++
