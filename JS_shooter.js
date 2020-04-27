3d1f7666058d2b3631773e0ec580648c7bcf86e4
Jazo74 <55430157+Jazo74@users.noreply.github.com>
2020-04-27 14:05:28 +0200

    x
    x

--- JS_shooter.js
+++ JS_shooter.js
@@ -1,13 +1,16 @@
 
 function main() {
   //"global"
-  const containerDiv = document.querySelector('.playground')
+  const containerDiv = document.querySelector('.playground');
+  
 
 
 
 
 
-  createTarget(5, containerDiv)
+  createTarget(5, containerDiv);
+const target = document.getElementsByClassName("target");
+target[0].style['margin-left'] = '1000px';
 }
 
 
@@ -20,24 +23,24 @@ function createTarget(amount, containerDiv) {
   var num = 1;
 
   for (let i = 0; i < amount; i++) {
-    
+    let rndVertical = Math.floor(Math.random() * 500);
     const img = document.createElement('img')
     img.setAttribute('class', 'target')
     img.setAttribute('src', 'https://pngimg.com/uploads/coronavirus/coronavirus_PNG7.png')
     img.setAttribute('id', `target_${num}`)
     img.setAttribute('width', '230')
     img.setAttribute('height', '150')
+    console.log(rndVertical);
     img.addEventListener('click', onHit, { once: true })
-
     containerDiv.appendChild(img)
+    img.style['margin-top'] = rndVertical;
     num++
   }
 
 }
 
 
-/*const target = document.getElementsByClassName("target");
-target[0].style['margin-left'] = '1000px';*/
+
 
 
 
