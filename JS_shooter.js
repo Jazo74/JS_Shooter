013b5711783761af510b3b38d02c45fab76b9b80
Erik-Aron-Szabo <erik.aron.szabo@gmail.com>
2020-04-27 13:17:07 +0200

    multiple targets added

--- JS_shooter.js
+++ JS_shooter.js
@@ -8,7 +8,14 @@ target[0].style['margin-left'] = '1000px';
 
 
 function main() {
-  createTarget(1)
+  //"global"
+  const containerDiv = document.querySelector('.playground')
+
+
+
+
+
+  createTarget(5, containerDiv)
   const target = document.querySelector('.target')
   target.addEventListener('click', onHit, { once: true })
 }
@@ -19,9 +26,22 @@ function onHit(event) {
   console.log(hitTarget)
 }
 
-function createTarget(amount) {
-  const img = document.createElement('img')
-  img.setAttribute('class', 'target')
+function createTarget(amount, containerDiv) {
+  var num = 1;
+
+  for (let i = 0; i < amount; i++) {
+    
+    const img = document.createElement('img')
+    img.setAttribute('class', 'target')
+    img.setAttribute('src', 'https://pngimg.com/uploads/coronavirus/coronavirus_PNG7.png')
+    img.setAttribute('id', `target_${num}`)
+    img.setAttribute('width', '230')
+    img.setAttribute('height', '150')
+
+    containerDiv.appendChild(img)
+    num++
+  }
+
 }
 
 
